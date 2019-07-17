1ad0f3a743d426380604b4a02b4252fcb18b3e83
mohammed ahmed abdullah <36166546+hamadahero2020@users.noreply.github.com>
2019-07-17 14:19:44 +0100

    Add files via upload

---
+++ my web sites/java.js
@@ -0,0 +1,16 @@
+var 
+    x=10, 
+    y=50,  
+    z=5; 
+
+
+alert(postMessage="take care baby")
+
+document.getElementsByClassName("hamada").innerHTML = x ;
+
+if (x+y == 50) {
+        console.log("good")
+    }else 
+        {
+            console.log("bad ")
+}
---
+++ my web sites/js/firstjava.js
@@ -0,0 +1,64 @@
+var // variable single line  
+    x , y=50 , z= 3 ; 
+
+if (x+y+z == 63)
+    {
+        console.log("true") ; 
+    }
+else{
+        console.log("false") ; 
+    }
+
+/* this is a multiline comment used when 
+there are many lines being commented */  
+
+
+var 
+
+    myPrice = 500 , myDiscount = 50  ;  
+
+Document.getElementById"product1".innerHTML = myPrice - myDiscount ; 
+Document.getElementById("product2").innerHTML =  myPrice- myDiscount ; 
+Document.getElementById("product3").innerHTML =  myPrice -myDiscount ;
+
+
+
+var hasDiscount = "true" ; 
+
+if (hasDiscount== true) 
+    {
+        var mainPrice = 355 ; 
+    }
+else {
+        var mainPrice = 455 ; 
+}
+
+var socialMedia =   ["facebook", "google", "youtube" ] ; 
+document.getElementById("price").innerHTML = socialMedia[0] ;
+
+
+// object {firstName:"osama" , lastName:"mohamed" } 
+
+
+var myInfo =  {firstName:"osama" , lastName:"mohamed" } ; 
+document.getElementById("price").innerHTML = myInfo.lastName ;   // لازم اكتب myInfo.  حاجة عشان يعرف 
+
+
+
+// string   
+
+// 32 is not equal "32"   32 is a number  ;  
+
+// null , Nan  not a number 
+
+// type of  "variable name " 
+
+
+
+
+
+
+
+
+
+
